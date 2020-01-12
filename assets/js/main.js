78d9fa937cbbbe1dedcfb08a84e5e60f459cca7a
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2020-01-12 22:32:35 +0600

    blog over veiw page fixing done

--- assets/js/main.js
+++ assets/js/main.js
@@ -91,7 +91,18 @@ if ($('.cookie-btn').length) {
 }
 
 
-
+$('[data-fancybox="gallery"]').fancybox({
+    loop : true,
+     buttons: [
+    "zoom",
+    "share",
+    "slideShow",
+    "fullScreen",
+    "download",
+    "thumbs",
+    "close"
+  ],
+});
 
 
 
